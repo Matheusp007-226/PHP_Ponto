ed2349954a982d15f532df1a82084aae4a18984f
Matheusp007-226 <83099808+Matheusp007-226@users.noreply.github.com>
2023-01-24 00:15:17 -0300

    Mudando o index.php de local

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-header("location: ./public/index.php");
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-?>
+if($uri === '/' || $uri === '' ||  $uri === '/index.php') {
+    $uri = '/day_records.php';
+}
+
+require_once(CONTROLLER_PATH . "/{$uri}");
